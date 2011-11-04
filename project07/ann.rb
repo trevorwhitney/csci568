61099a1ad41b7c05de53c20782edf415f43cc640
Trevor Whitney <trevorjwhitney@gmail.com>
2011-11-03 22:02:46 -0600

    fixed ANN so it runs

--- project07/ann.rb
+++ project07/ann.rb
@@ -1,18 +1,25 @@
+require 'pry'
 require_relative 'layer.rb'
 
 class ArtificialNeuralNetwork
-	attr_writer :input
-	attr_reader :output
+	attr_reader :output, :input
 
 	#initialize with an array containing number of neurons
 	#in each layer, including input and output
 	def initialize(layers)
+		@layers = Array.new
 		layers.each_with_index do |layer, i|
 			#calculate number of neurons in next layer, or 0
 			#if this is the last layer
 			next_layer = layers[i + 1] ? layers[i + 1] : 0
 			layer = Layer.new(layer, next_layer)
 			@layers << layer
+		end
+	end
+
+	def input=(input)
+		if input.size == @layers[0].neurons.size
+			@input = input
 		end
 	end
 
@@ -24,7 +31,10 @@ class ArtificialNeuralNetwork
 			if layer_index == 0
 				#create and array to hold the incoming values for each node
 				#on the next layer
-				@hidden = Array.new(@layers[1].neurons.size)
+				@hidden = Array.new
+				@layers[1].neurons.size.times do
+					@hidden << 0.0
+				end
 				
 				layer.neurons.each do |neuron|
 					#each "neuron" is an array of outgoing weights
@@ -42,21 +52,26 @@ class ArtificialNeuralNetwork
 				#we only need to bother if there's another layer
 				if new_size > 0
 					#create a new array to store incoming values for each neuron in the next layer
-					@new_hidden = Array.new(new_size)
+					@new_hidden = Array.new
+					new_size.times do
+						@new_hidden << 0.0
+					end
+
 					layer.neurons.each_with_index do |neuron, neuron_index|
 						#each "neuron" is an array of outgoing weights
 						neuron.each_with_index do |weight, weight_index|
-							@new_hidden[weight_index] += tanh(@hidden[neuron_index]) * weight
+							@new_hidden[weight_index] += Math.tanh(@hidden[neuron_index]) * weight
 						end
 					end
 
 					@hidden = @new_hidden
 				end
 			end
+		end
 
-			#when all layers have been calculted, the "incoming" values for our last "hidden"
-			#layer are really our output values
-			@output = @hidden
+		#when all layers have been calculted, the "incoming" values for our last "hidden"
+		#layer are really our output values
+		@output = @hidden
 	end
 
 	def train
